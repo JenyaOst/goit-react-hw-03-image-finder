71d551b1ec2687435d8b19102f5bfe893cc2c14e
JenyaOst <gjhit922@gmail.com>
2024-03-15 18:06:01 +0200

    searhbar + http

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Notiflix from 'notiflix';
+import { Searchbar } from './Searchbar/Searchbar';
+export default class App extends Component {
+  state = {
+    dataImages: null,
+    status: 'idle',
+    error: null,
+    searchValue: '',
+    page: 1,
+    loading: false,
+    isOpenModal: false,
+    modalData: null,
+  };
+  onSubmitForm = formData => {
+    if (formData.inputValue === this.state.searchValue) {
+      Notiflix.Notify.warning('You are making a similar request!', {
+        position: 'center-top',
+        width: '400px',
+        fontSize: '16px',
+      });
+      return;
+    }
+  };
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.onSubmitForm} />
+      </div>
+    );
+  }
+}
